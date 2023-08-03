ef78ac2e6d41d40a08fabc355fafa4545d8dabf9
ReturnerNaGithub <damsan1@gmail.com>
2023-08-03 21:05:46 +0200

    tuco

--- src/components/elements/MoralisLogo/MoralisLogo.tsx
+++ src/components/elements/MoralisLogo/MoralisLogo.tsx
@@ -7,7 +7,7 @@ const MoralisLogo = () => {
   return (
     <Image
       src={colorMode === 'dark' ? '/Moralis-DarkBG.svg' : '/Moralis-LightBG.svg'}
-      height={145}
+      height={45}
       width={150}
       alt="Moralis"
     />
